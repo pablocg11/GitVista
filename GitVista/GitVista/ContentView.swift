cc59ee6f35b8f297cd66dd12ed101c21b3505b01
Pablo Castro <pablocastrodb@gmail.com>
2024-12-13 23:02:06 +0100

    develop: added SwiftLint

---
+++ GitVista/GitVista/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitVista
+//
+//  Created by Pablo Castro on 13/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
